17db7b0c05d4cf9258a53123a037b62cf46b0a92
lama1991 <sario888sario@gmail.com>
2023-08-30 02:58:31 -0800

    commit iii

---
+++ database/migrations/2023_08_30_105533_create_complains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complains', function (Blueprint $table) {
+            $table->id();
+             $table->string('uuid')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complains');
+    }
+};
